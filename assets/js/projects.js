000818d45dc0ba6c55ca84c346393419a1aa1be9
NartPacarizi <kujtesar_paqarizi@hotmail.com>
2024-08-29 18:23:48 +0200

    portfolio updated

---
+++ assets/js/projects.js
@@ -0,0 +1,11 @@
+function showAlert(projectName) {
+    alert("You clicked on " + projectName + "!");
+}
+
+function hoverEffect(element) {
+    element.style.backgroundColor = '#ffb3d9';
+}
+
+function removeEffect(element) {
+    element.style.backgroundColor = '#ffccff';
+}
